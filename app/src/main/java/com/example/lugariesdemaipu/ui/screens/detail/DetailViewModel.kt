6dec7386819715a15a7440e391926e4ca6c6bf9b
jhonnylarry <152074791+jhonnylarry@users.noreply.github.com>
2025-11-12 20:00:09 -0300

    modificacion vista detalle

--- app/src/main/java/com/example/lugariesdemaipu/ui/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/lugariesdemaipu/ui/screens/detail/DetailViewModel.kt
@@ -2,21 +2,21 @@ package com.example.lugariesdemaipu.ui.screens.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.lugariesdemaipu.data.DatosMaipu
 import com.example.lugariesdemaipu.data.model.DetailState
-import com.example.lugariesdemaipu.LugarRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class DetailViewModel(private val lugarRepository: LugarRepository) : ViewModel() {
+class DetailViewModel : ViewModel() {
 
     private val _uiState = MutableStateFlow(DetailState())
     val uiState: StateFlow<DetailState> = _uiState.asStateFlow()
 
-    fun getLugar(id: Int) {
+    fun getLugar(id: String) {
         viewModelScope.launch {
-            val lugar = lugarRepository.getLugares().find { it.id == id }
+            val lugar = DatosMaipu.listaDeLugares.find { it.id == id }
             _uiState.value = DetailState(lugar = lugar)
         }
     }
